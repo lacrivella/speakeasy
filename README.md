52ef6cb19ef8251165df4558aa73d326b35be147
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-31 09:56:33 -0700

    set up configs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # speakeasy
+
+cocktail api from https://www.thecocktaildb.com/api.php
---
+++ test/tests.js
@@ -0,0 +1,18 @@
+import './html-equal.js';
+import './html-equal.test.js';
+import './html-to-DOM.test.js';
+
+
+
+// const test = QUnit.test;
+
+// test('time to test a function', function(assert) {
+//     //Arrange
+//     // Set up your parameters and expectations
+
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+
+//     //Assert
+//     assert.equal(true, false);
+// });
