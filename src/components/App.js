aa12d74ac56701cbe9d8530393f6ab5bd54cc3f0
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-31 16:44:49 -0700

    added loading gif to function

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import Component from './Component.js';
 import Header from './Header.js';
 import DrinkList from './DrinkList.js';
 import api from '../services/api.js';
+import Loading from './Loading.js';
 
 class App extends Component {
     render() {
@@ -10,16 +11,24 @@ class App extends Component {
         const header = new Header();
         const headerDOM = header.render();
 
+        
         const main = dom.querySelector('main');
         dom.insertBefore(headerDOM, main);
 
+        const loading = new Loading({ done: false });
+        main.appendChild(loading.render());
+        
         const drinkList = new DrinkList({ cocktail: {drinks: [] } });
         main.appendChild(drinkList.render());
 
         function loadDrink() {
+            loading.update({ done: false });
             api.getDrink()
                 .then(drinks => {
                     drinkList.update({ cocktail:drinks });
+                })
+                .finally(() => {
+                    loading.update({ done: true });
                 });
         }
         
