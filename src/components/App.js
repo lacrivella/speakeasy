1036dd72c9be18257b0b5d2d35737d7db79f4f34
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-31 10:20:39 -0700

    rendered header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import Component from './Component.js';
+import Header from './Header.js';
 
 class App extends Component {
+    render() {
+        const dom = this.renderDOM();
+
+        const header = new Header();
+        const headerDOM = header.render();
+
+        const main = dom.querySelector('main');
+        dom.insertBefore(headerDOM, main);
+
+        
+        return dom;
+    }
     renderTemplate() {
         return /*html*/ `
             <div>
-                <main>RAISE A GLASS YO</main>
+                <main></main>
             </div>
             
     `;
